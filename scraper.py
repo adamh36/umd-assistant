0ffbbec887058c5ac57e75f6a70403b1149aaf3f
adamh36 <adamhammoud04@gmail.com>
2026-04-11 19:01:58 -0400

    Add get_links function: extracts and filters UMD links from a page

--- scraper.py
+++ scraper.py
@@ -24,3 +24,30 @@ def get_page_text(url):
 
 print(get_page_text("https://umdearborn.edu/academics"))
 
+
+def get_links(url):
+
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        links = []
+        if response.status_code == 200:
+            for a_tag in soup.find_all("a", href = True): # finds every <a> tag that actually has an href, skips empty ones
+                href = a_tag['href'] #  pulls the URL string out of the tag, like opening an envelope to read the address
+
+                # Handle relative links 
+                if href.startswith("/"):
+                    href = "https://umdearborn.edu" + href
+
+            # Only keep umdearborn links
+                if "umdearborn.edu" in href:
+                    links.append(href)
+
+        return links
+    
+    except Exception as e:
+
+        print(f"ERROS: {e}")
+        return []
+
+print(get_links("https://umdearborn.edu/academics"))
