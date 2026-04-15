85743c0b0b13c6f3c6f797b8e9845a23ca159703
adamh36 <adamhammoud04@gmail.com>
2026-04-14 23:54:01 -0400

    Add load_documents and chunk_text functions

---
+++ ingest.py
@@ -0,0 +1,35 @@
+# Chunker 
+
+import os
+import chromadb  # the vector database where we store embeddings
+
+
+def load_documents():
+    documents = [] # start empty list to hold all the text documents we will read in
+    os.listdir('data/') # returns list of all filenames in data/ ( we can loop through it now)
+
+    for filename in os.listdir('data/'): # loop through each filename in the data/ directory
+        with open(f"data/{filename}", 'r') as file: # open the file for reading, using a context manager (with) to ensure it gets closed properly
+            text = file.read() # read the entire contents of the file into a string variable called text
+            documents.append(text) # add the text string to our documents list, so we end up with a list of all the text from all the files in data/ directory
+    
+    return documents
+
+docs = load_documents()
+print(len(docs))
+
+def chunk_text(text, chunk_size=500, overlap=50):# function to split a long text into smaller chunks, with some overlap between them
+    chunks = [] # start with an empty list to hold the chunks
+    start = 0 # initialize a variable to keep track of where we are in the text
+    while start < len(text):# loop until we've processed the entire text
+        end = min(start + chunk_size, len(text)) # calculate the end index for the current chunk, ensuring we don't go past the end of the text
+        chunk = text[start:end] # extract the chunk of text from the start index to the end index
+        chunks.append(chunk) # add the chunk to our list of chunks
+        start += chunk_size - overlap  # move forward by chunk_size minus the overlap
+    return chunks
+
+# Example usage:
+sample = "hello " * 200  # 1200 chars of text
+chunks = chunk_text(sample)
+print(len(chunks))
+print(chunks[0][:50])  # first chunk preview
